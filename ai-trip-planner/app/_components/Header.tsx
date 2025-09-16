2a6f0dfe6ef47dbaa61686387357bca171f6efb9
Suraj Kumar Mandal <surajkumarmandal2003@gmail.com>
2025-09-16 20:39:02 +0530

    Landing Screen updated

---
+++ ai-trip-planner/app/_components/Header.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import path from 'path'
+import React from 'react'
+
+const menuOptions=[
+    {
+        name:'Home',
+        path:'/'
+    },
+    {
+        name:'Pricing',
+        path:'/pricing'
+    },
+    {
+        name:'Contact us',
+        path:'/contact-us'
+    }
+]
+
+function Header() {
+  return (
+    <div className='flex justify-between items-center p-4'>
+        {/* Logo */}
+        <div className='flex gap-2 items-center'>
+            <Image src={'/logo.svg'} alt='logo' width={30} height={30} />
+            <h2 className='font-bold text-2xl'>WayFarer</h2>    
+        </div>
+        {/* Menu Options */}
+        <div className='flex gap-8 items-center'>
+            {menuOptions.map((menu, index)=>(
+                <Link href={menu.path}>
+                    <h2 className='text-lg hover:scale-105 transition-all hover:text-primary'>{menu.name}</h2>
+                </Link>
+            ))}
+        </div>
+
+        {/* Get Started Button */}
+        <Button>Get Started</Button>
+    </div>
+  )
+}
+
+export default Header
---
+++ ai-trip-planner/app/_components/Hero.tsx
@@ -0,0 +1,74 @@
+import { Button } from '@/components/ui/button'
+import { HeroVideoDialog } from '@/components/ui/hero-video-dialog'
+import { Textarea } from '@/components/ui/textarea'
+import { ArrowDown, Globe2, Landmark, Plane, Send } from 'lucide-react'
+import { title } from 'process'
+import React from 'react'
+
+const suggestions = [
+    {
+        title:'Create New Trip',
+        icon:<Globe2 className='text-blue-400 h-5 w-5'/>,
+    },
+    {
+        title:'Inspire me where to go',
+        icon:<Plane className='text-green-400 h-5 w-5'/>,
+    },
+    {
+        title:'Discover Hidden Gems',
+        icon:<Landmark className='text-orange-400 h-5 w-5'/>,
+    },
+    {
+        title:'Adventure Destination',
+        icon:<Globe2 className='text-blue-400 h-5 w-5'/>,
+    }
+]
+
+function Hero() {
+  return (
+    <div className='mt-24 w-full flex justify-center'>
+        {/* Content  */}
+        <div className='max-w-3xl w-full text-center space-y-6'>
+            <h1 className='text-xl md:text-5xl font-bold '>Hey, I'm your personal <span className='text-primary'>Trip Planner</span></h1>
+            <p className='text-lg'>Tell me what you want, and I'll handle the rest: Flights, Hotels, trip planner - all in seconds</p>
+        
+ 
+        {/* Input Box */}
+        <div>
+            <div className='border rounded-2xl p-4 relative'>
+                <Textarea placeholder='Create a trip for Paris from New York' 
+                className='w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none'
+                />
+                <Button size= {'icon'} className='absolute bottom-6 right-6'>
+                    <Send className='h-4 w-4'/>
+                </Button>
+            </div>
+        </div>
+
+        {/* suggestion list */}
+        <div className='flex gap-5'>
+            {suggestions.map((suggestions,index)=>(
+                <div key={index} className='flex items-center gap-2 border rounded-full p-2 cursor-pointer hover:bg-primary hover:text-white'>
+                    {suggestions.icon}
+                    <h2 className='text-sm'>{suggestions.title}</h2>
+                </div>
+            ))}
+        </div>
+        <div className='flex items-center justify-center flex-col'>
+            <h2 className='my-7 mt-14 flex gap-2 text-center'>Not Sure where to start? <strong>See how it works</strong> <ArrowDown /></h2>
+
+            {/* Video section */}
+            <HeroVideoDialog
+                className="block dark:hidden"
+                animationStyle="from-center"
+                videoSrc="https://www.example.com/dummy-video"
+                thumbnailSrc="https://mma.prnewswire.com/media/2401528/1_MindtripProduct.jpg?p=facebook"
+                thumbnailAlt="Dummy Video Thumbnail"
+            />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
