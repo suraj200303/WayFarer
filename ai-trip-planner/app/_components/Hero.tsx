bf7a8f8cc72a97ca53e6d61eb228bfe4be372073
Arup Patra <aruppatra2004@gmail.com>
2025-10-02 11:20:31 +0530

    ai model setup completed

--- ai-trip-planner/app/_components/Hero.tsx
+++ ai-trip-planner/app/_components/Hero.tsx
@@ -9,7 +9,7 @@ import { useRouter } from 'next/navigation'
 import { title } from 'process'
 import React from 'react'
 
-const suggestions = [
+export const suggestions = [
     {
         title:'Create New Trip',
         icon:<Globe2 className='text-blue-400 h-5 w-5'/>,
@@ -70,9 +70,9 @@ function Hero() {
                 </div>
             ))}
         </div>
+
         <div className='flex items-center justify-center flex-col'>
             <h2 className='my-7 mt-14 flex gap-2 text-center'>Not Sure where to start? <strong>See how it works</strong> <ArrowDown /></h2>
-
             {/* Video section */}
             <HeroVideoDialog
                 className="block dark:hidden"
--- ai-trip-planner/app/create-new-trip/_components/ChatBox.tsx
+++ ai-trip-planner/app/create-new-trip/_components/ChatBox.tsx
@@ -1,33 +1,82 @@
 "use client"
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
-import { Send } from 'lucide-react'
-import React from 'react'
+import axios from 'axios'
+import { Loader, Send } from 'lucide-react'
+import React, { useState } from 'react'
+import EmptyBoxState from './EmptyBoxState'
+
+type Message = {
+    role: string,
+    content: string,
+}
 
 function ChatBox() {
 
-  const onSend = () => {}
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [userInput, setUserInput] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const onSend = async () => {
+    if(!userInput?.trim()) {
+        return;
+    }
+
+    setLoading(true);
+    setUserInput('');
+    const newMessage: Message = {
+        role: 'user',
+        content: userInput.trim(),
+    }
+    setMessages((prev:Message[]) => [...prev, newMessage]);
+    const result = await axios.post('api/aimodel', {
+        messages: [...messages, newMessage]
+    })
+    
+    setMessages((prev:Message[]) => [...prev, {
+        role: 'assistant',
+        content: result.data.resp,
+    }]);
+
+    console.log(result.data);
+    setLoading(false);
+  }
 
   return (
     <div className='h-[85vh] flex flex-col'> 
+        {messages.length === 0 && 
+            <EmptyBoxState 
+            onSelectOption = {(v: string) => { setUserInput(v); onSend() }}
+            />
+        }
         {/* Display Message */}
         <section className='flex-1 overflow-y-auto p-4'>
-            <div className='flex justify-end mt-2'>
-                <div className='max-w-lg bg-primary text-white px-4 py-2 rounded-lg'>
-                    User msg
+            {messages.map((msg:Message, index) => (
+                msg.role === 'user' ? 
+                <div className='flex justify-end mt-2' key={index}>
+                    <div className='max-w-lg bg-primary text-white px-4 py-2 rounded-lg capitalize'>
+                        {msg.content}
+                    </div>
+                </div> :
+                <div className='flex justify-start mt-2' key={index}>
+                    <div className='max-w-lg bg-gray-200 text-black px-4 py-2 rounded-lg'>
+                        {msg.content}
+                    </div>
                 </div>
-            </div>
-            <div className='flex justify-start mt-2'>
+            ))} 
+
+            {loading && <div className='flex justify-start mt-2'>
                 <div className='max-w-lg bg-gray-200 text-black px-4 py-2 rounded-lg'>
-                    Ai Agent Msg
+                    <Loader className='animate-spin'/>
                 </div>
-            </div>
+            </div>}
         </section>
         {/* User input */}
         <section>
             <div className='border rounded-2xl p-4 relative'>
-                <Textarea placeholder='Create a trip for Paris from New York' 
-                className='w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none'
+                <Textarea placeholder='Start your trip planing journey...' 
+                    className='w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none capitalize'
+                    onChange={(event) => setUserInput(event.target.value??'')}
+                    value={userInput}
                 />
                 <Button size= {'icon'} className='absolute bottom-6 right-6' onClick={()=>onSend()}>
                     <Send className='h-4 w-4'/>
